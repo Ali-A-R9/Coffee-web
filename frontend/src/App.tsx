0614e33ede2d66b5d3354cd82e2c5dddfdcec2c3
Ali <s202281300@kfupm.edu.sa>
2026-03-04 23:51:36 +0300

    main connections

---
+++ frontend/src/App.tsx
@@ -0,0 +1,71 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import CafeProfile from "./pages/CafeProfile";
+import ProtectedRoute from "./components/ProtectedRoute";
+import Menu from "./pages/Menu";
+import PublicPreview from "./pages/PublicPreview";
+import Admin from "./pages/Admin";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+
+        {/* Public Routes */}
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+
+        {/* Protected Routes */}
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/cafe-profile"
+          element={
+            <ProtectedRoute>
+              <CafeProfile />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/menu"
+          element={
+            <ProtectedRoute>
+              <Menu />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/preview"
+          element={
+            <ProtectedRoute>
+              <PublicPreview />
+            </ProtectedRoute>
+          }
+        />
+
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
